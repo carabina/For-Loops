9fe636c7b294073ca12ccf324997f186862424fa
Eliot Arntz <arntzel@gmail.com>
2013-09-27 19:59:32 +0200

    update

--- For Loops/CCViewController.m
+++ For Loops/CCViewController.m
@@ -17,7 +17,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    /* To create a for loop we must first use an integer to set our initial value.  Next we need to define the limitations of the for loop.  What is the highest or lowest value that we iterate to.  Finally, we need to tell the for loop how much to add or subtract from our initial value each time we evaluate all of the code in the for loop */
+    
+//to uncoment many lines of code at a single time
+    
+//    for (int meditationHours = 1; meditationHours <= 100; meditationHours ++){
+//        NSLog(@"I am getting more enlightened");
+//    }
+    int strandOfWheat = 1;
+    for (int i = 2; i <= 30; i ++){
+        strandOfWheat = strandOfWheat * 2;
+        NSLog(@"%i", strandOfWheat);
+    }
 }
 
 - (void)didReceiveMemoryWarning
